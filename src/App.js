092238f860ce32831835a4aeac7e176a11277a9b
Wei Yang <wei@weizilla.com>
2020-02-21 20:40:53 -0600

    More stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>hello</h1>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>hello react</h1>
+                <Person name="Max" age="28"/>
+                <Person name="Manu" age="29">My hobby is racing</Person>
+                <Person name="Bob" age="28"/>
+            </div>
+        );
+    }
 }
 
 export default App;
