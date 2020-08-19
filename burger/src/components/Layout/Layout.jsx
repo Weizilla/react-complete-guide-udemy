5dd5e10d18cc684c73461fc068c1f15382691745
Wei Yang <wei@weizilla.com>
2020-08-19 15:18:16 -0500

    Side drawer

--- burger/src/components/Layout/Layout.jsx
+++ burger/src/components/Layout/Layout.jsx
@@ -3,12 +3,14 @@ import PropTypes from "prop-types";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => {
   const { children } = props;
   return (
     <Aux>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>
         {children}
       </main>
